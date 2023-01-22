40994d9d550e70f2ed516a6d5f0565059205add0
kavin-kumar10 <kavinkumar.prof@gmail.com>
2023-01-22 23:59:48 +0530

    preloader

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import Homepage from "./Homepage";
 import Catagory from "./Catagory";
+import Preloader from "../Preloader";
 
 const Main = () =>{
+    const [isLoading,setLoading] = useState(true);
+    useEffect(()=>{
+      setLoading(true);
+      setTimeout(()=>{
+        setLoading(false);
+      },2000);
+    },[])
     return(
-        <div className="Main">
+        <div>
+             {
+        isLoading?(<Preloader/>)
+        :
+        (<div className='Main'>
             <Homepage/>
             <Catagory/>
+        </div>)
+      }
+
         </div>
     )
 }
